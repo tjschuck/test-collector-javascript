96187f3144bc33676c5bb5209941ca22b116b9b9
T.J. Schuck <tj@tjschuck.com>
2023-10-11 10:08:52 -0400

    console out the error

--- playwright/reporter.js
+++ playwright/reporter.js
@@ -93,14 +93,11 @@ class PlaywrightBuildkiteAnalyticsReporter {
    * @param {TestResult} testResult
    */
   analyticsFailureMessages(testResult) {
-    console.log("---enter analyticsFailureMessages")
     if (testResult.error == undefined) return [];
-    console.log("---error is defined in analyticsFailureMessages")
+    console.log("---testResult.error: ", testResult.error)
 
     console.log("---testResult.error.stack: ", testResult.error.stack)
     console.log("---testResult.error.snippet: ", testResult.error.snippet)
-    console.log("---stripAnsi(testResult.error.stack): ", stripAnsi(testResult.error.stack))
-    console.log("---stripAnsi(testResult.error.snippet): ", stripAnsi(testResult.error.snippet))
 
     const stack = stripAnsi(testResult.error.stack).split("\n");
     const snippet = stripAnsi(testResult.error.snippet).split("\n");
