e73276f0fc62e26e8c00f350cdbde78b07892af1
T.J. Schuck <tj@tjschuck.com>
2023-10-11 10:35:24 -0400

    console out token info

--- util/uploadTestResults.js
+++ util/uploadTestResults.js
@@ -5,6 +5,8 @@ const CHUNK_SIZE = 5000
 const DEFAULT_BUILDKITE_ANALYTICS_BASE_URL = 'https://analytics-api.buildkite.com/v1/uploads'
 
 const uploadTestResults = (env, results, options, done) => {
+  console.log("------------- process.env.BUILDKITE_ANALYTICS_TOKEN: ", process.env.BUILDKITE_ANALYTICS_TOKEN)
+  console.log("------------- options: ", options)
   const buildkiteAnalyticsToken = options?.token || process.env.BUILDKITE_ANALYTICS_TOKEN
   let data
 
